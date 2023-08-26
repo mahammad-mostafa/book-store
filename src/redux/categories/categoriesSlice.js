266d94d522e3f012cefdf4cb9a6224b89307c3c4
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-08-27 01:07:26 +0300

    Implemented check action & reducer

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const states = { list: [], status: 'Under Construction' };
+
+const categoriesReducer = createSlice({
+  name: 'categories',
+  initialState: states,
+  reducers: {
+    check: (state) => state.status,
+  },
+});
+
+export const { check } = categoriesReducer.actions;
+
+export default categoriesReducer.reducers;
