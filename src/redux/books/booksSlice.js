50195f17783a5477961769edc63f507fe66523fe
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-09-01 00:03:32 +0300

    Used thunks & axios for api fetching with reducers

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,45 +1,70 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 import { v4 } from 'uuid';
 
 const states = {
-  list: [
-    {
-      id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+  list: [],
+  updated: true,
+  loading: false,
+  error: undefined,
 };
+
+axios.defaults.baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/OzVBqnExNMneqSrRACVA/books/';
+
+export const bookGet = createAsyncThunk('books/get', async (_, { rejectWithValue }) => {
+  try {
+    return (await axios.get()).data;
+  } catch (error) {
+    return rejectWithValue(error.message);
+  }
+});
+
+export const bookPost = createAsyncThunk('books/post', async (book, { rejectWithValue }) => {
+  try {
+    const result = (await axios.post('', { item_id: v4(), ...book })).data;
+    if (result !== 'Created') {
+      return rejectWithValue(result);
+    }
+    return result;
+  } catch (error) {
+    return rejectWithValue(error.message);
+  }
+});
+
+export const bookDelete = createAsyncThunk('books/delete', async (id, { rejectWithValue }) => {
+  try {
+    const result = (await axios.delete(id, { item_id: id })).data;
+    if (result !== 'The book was deleted successfully!') {
+      return rejectWithValue(result);
+    }
+    return result;
+  } catch (error) {
+    return rejectWithValue(error.message);
+  }
+});
 
 const bookReducer = createSlice({
   name: 'books',
   initialState: states,
-  reducers: {
-    add: (state, action) => {
-      const book = {
-        id: v4(), title: action.payload.title, author: action.payload.author,
-      };
-      state.list.push(book);
-    },
-    remove: (state, action) => {
-      state.list = state.list.filter((book) => book.id !== action.payload);
-    },
+  extraReducers: (builder) => {
+    builder.addCase(bookGet.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.updated = false;
+      state.list = Object.keys(payload).map((key) => ({ id: key, ...payload[key][0] }));
+    });
+    builder.addMatcher(({ type }) => type.endsWith('pending'), (state) => {
+      state.loading = true;
+    });
+    builder.addMatcher(({ type }) => !type.includes('get') && type.endsWith('fulfilled'), (state) => {
+      state.updated = true;
+    });
+    builder.addMatcher(({ type }) => type.endsWith('rejected'), (state, { error }) => {
+      if (error.name !== 'AbortError') {
+        state.loading = false;
+        state.error = error.name;
+      }
+    });
   },
 });
-
-export const { add, remove } = bookReducer.actions;
 
 export default bookReducer.reducer;
