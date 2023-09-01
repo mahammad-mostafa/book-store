65e455f6ea0389fac3900575abf6933eb32b351f
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-09-02 00:27:14 +0300

    Styled categories component

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -1,14 +1,20 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { select } from '../redux/categories/categoriesSlice';
+import Styles from '../styles/categories.module.css';
 
 const Categories = () => {
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.categories.list);
   const selection = useSelector((state) => state.categories.selection);
   return (
-    <section>
-      <h2>Books Categories</h2>
-      <select onChange={(event) => dispatch(select(event.target.value))} defaultValue={selection}>
+    <section className={Styles.section}>
+      <h2 className={Styles.title}>Filter Books By Category</h2>
+      <select
+        className={Styles.select}
+        name="category"
+        onChange={(event) => dispatch(select(event.target.value))}
+        defaultValue={selection}
+      >
         <option value="all">all</option>
         {categories.map((item) => <option key={item} value={item}>{item}</option>)}
       </select>
