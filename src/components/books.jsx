002b7f85244622683862f56078a192fdd754d294
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-09-02 00:29:24 +0300

    Restructured books component and styled it

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -1,28 +1,21 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { bookGet, bookPost } from '../redux/books/booksSlice';
+import { bookGet } from '../redux/books/booksSlice';
+import Styles from '../styles/books.module.css';
+import Indicator from './indicator';
 import Book from './book';
+import Form from './form';
 
 const Books = () => {
   const dispatch = useDispatch();
   let books = useSelector((state) => state.books.list);
+  const error = useSelector((state) => state.books.error);
+  const loading = useSelector((state) => state.books.loading);
   const updated = useSelector((state) => state.books.updated);
-  const categories = useSelector((state) => state.categories.list);
   const selection = useSelector((state) => state.categories.selection);
   if (selection !== 'all') {
-    books = books.filter((item) => item.category !== selection);
+    books = books.filter((item) => item.category === selection);
   }
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    if (form.title.value !== '' && form.author.value !== '' && form.category.value !== '') {
-      const book = {
-        title: form.title.value, author: form.author.value, category: form.category.value,
-      };
-      dispatch(bookPost(book));
-      form.reset();
-    }
-  };
 
   useEffect(() => {
     if (updated) {
@@ -32,19 +25,19 @@ const Books = () => {
     return undefined;
   }, [dispatch, updated]);
 
+  if (loading) {
+    return <section className={Styles.section}><Indicator loading="true" /></section>;
+  }
+
   return (
-    <section>
-      <h2>Books List</h2>
-      <div>{books.map((item) => <Book key={item.id} book={item} />)}</div>
-      <form onSubmit={handleSubmit}>
-        <h3>Add New Book</h3>
-        <input type="text" placeholder="Book title" name="title" autoComplete="on" required />
-        <input type="text" placeholder="Book author" name="author" autoComplete="on" required />
-        <select name="category">
-          {categories.map((item) => <option key={item} value={item}>{item}</option>)}
-        </select>
-        <button type="submit">ADD BOOK</button>
-      </form>
+    <section className={Styles.section}>
+      {error === undefined && books.length > 0 ? (
+        <div className={Styles.list}>
+          {books.map((item) => <Book key={item.id} book={item} />)}
+        </div>
+      ) : <Indicator error={error} length={books.length} />}
+      <hr className={Styles.line} />
+      <Form />
     </section>
   );
 };
