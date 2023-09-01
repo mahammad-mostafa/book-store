90641e8492778ef1a7c7a7f5d1ea12e4e37c88ae
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-09-02 00:31:07 +0300

    Created & styled jsx structure for book component

--- src/components/book.jsx
+++ src/components/book.jsx
@@ -1,15 +1,32 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { bookDelete } from '../redux/books/booksSlice';
+import Styles from '../styles/book.module.css';
+import Chapter from './chapter';
 
 const Book = ({ book }) => {
   const dispatch = useDispatch();
   return (
-    <article>
-      <h3>{book.title}</h3>
-      <p>{book.author}</p>
-      <p>{book.category}</p>
-      <button type="button" onClick={() => dispatch(bookDelete(book.id))}>Delete</button>
+    <article className={Styles.article}>
+      <div className={Styles.details}>
+        <div>
+          <p className={Styles.category}>{book.category}</p>
+          <h3 className={Styles.title}>{book.title}</h3>
+          <p className={Styles.author}>{book.author}</p>
+        </div>
+        <ul className={Styles.buttons}>
+          <li>
+            <button type="button">Comments</button>
+          </li>
+          <li>
+            <button type="button" onClick={() => dispatch(bookDelete(book.id))}>Remove</button>
+          </li>
+          <li>
+            <button type="button">Edit</button>
+          </li>
+        </ul>
+      </div>
+      <Chapter progress={Math.floor(book.title.length / 0.5)} chapter={book.title.length} />
     </article>
   );
 };
